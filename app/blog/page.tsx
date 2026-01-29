a90c02cdbd34bd149ba91980492f526d0377a03c
Shuzo Matsumura <shumatts@gmail.com>
2026-01-29 13:23:34 +0900

    Move blog list to home and redirect /blog to /

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,6 @@
 // app/blog/page.tsx
 import { redirect } from "next/navigation";
 
-/**
- * /blog は使わない
- * すべてのアクセスを /（ブログ一覧＝ホーム）へ集約する
- */
 export default function BlogPage() {
   redirect("/");
 }
